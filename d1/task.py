e368b128bab3fc1d294afeababbc330078657d39
ibednorz <bednorz.ireneusz@gmail.com>
2024-11-04 23:57:19 +0100

    day 1, variables

---
+++ d1/task.py
@@ -0,0 +1,17 @@
+# print("hello from task.py")
+
+# print("hello from task.py\nhello from task.py")
+
+# print("hello" + " from task.py")
+# print("hello" + "from task.py" + "hello from task.py")
+
+# input('Press Enter to exit')
+
+# name = input("What is your name? ")
+
+# print("Hello, " + name + "!")
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f'Your band name could be {city} {pet}')
